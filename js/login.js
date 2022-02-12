a763e7e66ce8530380a65cd5ec8000e89747af6b
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2022-02-12 11:44:29 +1000

    add login section

---
+++ js/login.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let elForm = document.querySelector(".login__form");
+let elLogin = document.querySelector(".input-login");
+let elPassword = document.querySelector(".input-password");
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  let inputlogin = elLogin.value;
+  let inputPassword = elPassword.value;
+
+  fetch("https://reqres.in/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: inputlogin,
+      password: inputPassword,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.token) {
+        loginError.classList.add("visually-hidden");
+        window.localStorage.setItem("token", data.token);
+        window.location.replace("index.html");
+      } else {
+        loginError.classList.remove("visually-hidden");
+        elLogin.value = null;
+        elPassword.value = null;
+      }
+    });
+});
+
+// body: JSON.stringify({
+//     email: "eve.holt@reqres.in",
+//     password: "cityslicka",
+//   }),
